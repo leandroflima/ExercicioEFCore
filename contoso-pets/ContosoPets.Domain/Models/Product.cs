acc1ee7bcad197e1d53cf5f98cd3c81792e86741
Groo O Errante <grooflima@gmail.com>
2019-10-03 21:17:42 -0300

    versão inicial

---
+++ contoso-pets/ContosoPets.Domain/Models/Product.cs
@@ -0,0 +1,22 @@
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ContosoPets.Domain.Models
+{
+    public partial class Product
+    {
+        public Product()
+        {
+            ProductOrders = new HashSet<ProductOrder>();
+        }
+
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Column(TypeName = "decimal(18, 2)")]
+        public decimal Price { get; set; }
+
+        public ICollection<ProductOrder> ProductOrders { get; set; }
+    }
+}
